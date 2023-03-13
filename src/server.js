5a63f540e9c139bd0080e35ade0f645a63e8c702
amir-skoric <89415335+amir-skoric@users.noreply.github.com>
2023-03-13 20:17:08 +0100

    added pico css and made changes to server

--- src/server.js
+++ src/server.js
@@ -12,6 +12,9 @@ const userCollection = require('./database')
 //"Initialize" express
 const app = express();
 
+//For importing CSS
+app.use('/public', express.static('public'))
+
 //Middleware for Express JSON
 app.use(express.json());
 app.use(express.urlencoded ({extended: false}))
@@ -20,13 +23,12 @@ app.use(express.urlencoded ({extended: false}))
 app.use(sessions({
     secret: process.env.KEY,
     saveUninitialized: true,
+    resave: true,
     cookie: { 
-        maxAge: 10000 },
-    resave: false 
+        maxAge: 3600000
+    }
 }));
 
-//Session variable
-var session;
 
 //App listener (start server)
 app.listen(process.env.DEV_PORT, () => console.log("Started server on localhost:" + process.env.DEV_PORT));
@@ -35,27 +37,38 @@ app.listen(process.env.DEV_PORT, () => console.log("Started server on localhost:
 app.engine('.hbs', exphbs.engine({extname: '.hbs', defaultLayout: "", layoutsDir: ""}));
 app.set('view engine', '.hbs');
 
+//Middleware function that makes sure that you can't "back-track" on the website
+function noCache(req, res, next) {
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    res.header('Expires', '-1');
+    res.header('Pragma', 'no-cache');
+    next();
+  }
+
 //Login authentication
 function auth (req, res, next) {
-    if (req.session.authenticated) {
-        res.redirect('/')
+    if (req.session.isAuthenticated) {
+        next();
     }
     else {
-        res.redirect('/login')
+        res.redirect('/login');
     }
 }
 
 //Routes
-app.get('/', auth, (req, res) => {
-    res.render('index')
+app.get('/', auth, noCache, async (req, res) => {
+    const user = await userCollection.findOne( { username: req.session.userid});
+    res.render('index', { user: user.username})
 })
 
-app.get('/login', (req, res) => {
-    res.render('login')
+app.get('/login', noCache, (req, res) => {
+    res.render('login');
+    req.session.isAuthenticated = false;
 })
 
-app.get('/signup', (req, res) => {
-    res.render('signup')
+app.get('/signup', noCache, (req, res) => {
+    res.render('signup');
+    req.session.isAuthenticated = false;
 })
 
 //Database insertions
@@ -69,8 +82,8 @@ app.post('/signup', async (req, res) => {
             password: req.body.password
         }
     //Checking if a user with the same username or email exists
-    const checkUsername = await userCollection.exists({username: req.body.username})
-    const checkEmail = await userCollection.exists({email: req.body.email})
+    const checkUsername = await userCollection.exists({username: req.body.username});
+    const checkEmail = await userCollection.exists({email: req.body.email});
     //If the user doesn't exist, insert into the database
     if (checkUsername === null && checkEmail === null) {
         //Hash password
@@ -82,35 +95,38 @@ app.post('/signup', async (req, res) => {
     }
     else {
         //error if username or email is in use
-        res.send('Username or E-mail already in use!')
+        return res.json({ error: "Username or E-mail already in use!" });
     }
 })
 
 //Login
 app.post('/login', async (req, res) => {
     try {
-        const user = await userCollection.findOne({username: req.body.username})
+        const user = await userCollection.findOne({username: req.body.username});
     
     //if no user is found, an epic fail will ensue
     if (!user) {
-        return res.send("Wrong user credentials!")
+        return res.json({ error: "Wrong user credentials!" });
     }
     else {
     //Comparing the hashed passwords, and if they match, set the session
     if ((await bcrypt.compare(req.body.password, user.password))) {
         req.session.userid = req.body.username;
-        req.session.authenticated = true;
-        res.redirect('/')
+        req.session.isAuthenticated = true;
+        res.redirect('/');
         }
     }
     }
     catch {
-        res.send("Wrong user credentials!")
+        return res.json({ error: "Wrong user credentials!" });
     }
 })
 
 //Sign-out (delete session)
 app.get('/signout', (req, res) => {
     req.session.destroy();
-    res.redirect('/login')
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    res.header('Expires', '-1');
+    res.header('Pragma', 'no-cache');
+    res.redirect('/login');
 })
